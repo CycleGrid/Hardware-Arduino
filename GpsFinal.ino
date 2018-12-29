d76b14d89d2bd71a155eca92c2b3ec1c612e0a53
CycleGrid <46125567+CycleGrid@users.noreply.github.com>
2018-12-29 19:42:05 +0530

    Add files via upload
    GPS Data from Neo6m to Arduino

---
+++ GpsFinal.ino
@@ -0,0 +1,31 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+ */
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+
+TinyGPSPlus gps;
+// The serial connection to the GPS module
+SoftwareSerial ss(4, 3);
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(9600);
+}
+
+void loop(){
+  while (ss.available() > 0){
+    // get the byte data from the GPS
+    //byte gpsData = ss.read();
+    gps.encode(ss.read());
+    //Serial.write(gpsData);
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
+    }
+  }
+}
